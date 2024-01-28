b13007ee260bcdc8df44159be9f195f73a2bc8ad
Abhishek singh chauhan <avi7000201383@gmail.com>
2024-01-29 01:03:39 +0530

    changed home page

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,104 +1,173 @@
-import React from "react";
-import { IoIosArrowForward } from "react-icons/io";
+import { useState } from "react";
+import { Dialog } from "@headlessui/react";
+// import { ArchiveIcon } from "@heroicons/react/outline";
+import { motion } from "framer-motion";
+import { FaReact } from "react-icons/fa6";
+import { IoLogoJavascript } from "react-icons/io5";
+import { SiJavascript, SiTailwindcss } from "react-icons/si";
+import { SiRedux } from "react-icons/si";
+
+const navigation = [
+  { name: "About Me", href: "#" },
+  { name: "Projects", href: "#" },
+  { name: "Experience", href: "#" },
+  { name: "Contact Me", href: "#" },
+];
+
 const Hero = () => {
+  const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
+
   return (
-    <div className="relative isolate overflow-hidden bg-gray-900">
-      <svg
-        className="absolute inset-0 -z-10 h-full w-full stroke-white/10 [mask-image:radial-gradient(100%_100%_at_top_right,white,transparent)]"
-        aria-hidden="true"
-      >
-        <defs>
-          <pattern
-            id="983e3e4c-de6d-4c3f-8d64-b9761d1534cc"
-            width={200}
-            height={200}
-            x="50%"
-            y={-1}
-            patternUnits="userSpaceOnUse"
-          >
-            <path d="M.5 200V.5H200" fill="none" />
-          </pattern>
-        </defs>
-        <svg x="50%" y={-1} className="overflow-visible fill-gray-800/20">
-          <path
-            d="M-200 0h201v201h-201Z M600 0h201v201h-201Z M-400 600h201v201h-201Z M200 800h201v201h-201Z"
-            strokeWidth={0}
-          />
-        </svg>
-        <rect
-          width="100%"
-          height="100%"
-          strokeWidth={0}
-          fill="url(#983e3e4c-de6d-4c3f-8d64-b9761d1534cc)"
-        />
-      </svg>
-      <div
-        className="absolute left-[calc(50%-4rem)] top-10 -z-10 transform-gpu blur-3xl sm:left-[calc(50%-18rem)] lg:left-48 lg:top-[calc(50%-30rem)] xl:left-[calc(50%-24rem)]"
-        aria-hidden="true"
-      >
-        <div
-          className="aspect-[1108/632] w-[69.25rem] bg-gradient-to-r from-[#80caff] to-[#4f46e5] opacity-20"
-          style={{
-            clipPath:
-              "polygon(73.6% 51.7%, 91.7% 11.8%, 100% 46.4%, 97.4% 82.2%, 92.5% 84.9%, 75.7% 64%, 55.3% 47.5%, 46.5% 49.4%, 45% 62.9%, 50.3% 87.2%, 21.3% 64.1%, 0.1% 100%, 5.4% 51.1%, 21.4% 63.9%, 58.9% 0.2%, 73.6% 51.7%)",
-          }}
-        />
-      </div>
-      <div className="mx-auto max-w-7xl px-6 pb-24 pt-10 sm:pb-32 lg:flex lg:px-8 lg:py-40">
-        <div className="mx-auto max-w-2xl flex-shrink-0 lg:mx-0 lg:max-w-xl lg:pt-8">
-          <img
-            className="h-11"
-            src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=500"
-            alt="Your Company"
-          />
-          <div className="mt-24 sm:mt-32 lg:mt-16">
-            <a href="#" className="inline-flex space-x-6">
-              <span className="rounded-full bg-indigo-500/10 px-3 py-1 text-sm font-semibold leading-6 text-indigo-400 ring-1 ring-inset ring-indigo-500/20">
-                What's new
-              </span>
-              <span className="inline-flex items-center space-x-2 text-sm font-medium leading-6 text-gray-300">
-                <span>Just shipped v1.0</span>
-                <IoIosArrowForward
-                  className="h-5 w-5 text-gray-500"
-                  aria-hidden="true"
-                />
-              </span>
+    <div className="bg-neutral-950 min-h-screen">
+      <header className="absolute inset-x-0 top-0 z-50">
+        <nav
+          className="flex items-center justify-between p-6 lg:px-8"
+          aria-label="Global"
+        >
+          <div className="flex lg:flex-1">
+            <a href="#" className="-m-1.5 p-1.5">
+              <span className="sr-only">Your Company</span>
+              <img
+                className="h-8 w-auto"
+                src="https://tailwindui.com/img/logos/mark.svg?color=lime&shade=600"
+                alt=""
+              />
             </a>
           </div>
-          <h1 className="mt-10 text-4xl font-bold tracking-tight text-white sm:text-6xl">
-            Deploy to the cloud with confidence
-          </h1>
-          <p className="mt-6 text-lg leading-8 text-gray-300">
-            Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui
-            lorem cupidatat commodo. Elit sunt amet fugiat veniam occaecat
-            fugiat aliqua.
-          </p>
-          <div className="mt-10 flex items-center gap-x-6">
-            <a
-              href="#"
-              className="rounded-md bg-indigo-500 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-400 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-400"
+          <div className="flex lg:hidden">
+            <button
+              type="button"
+              className="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-100"
+              onClick={() => setMobileMenuOpen(true)}
             >
-              Get started
-            </a>
-            <a href="#" className="text-sm font-semibold leading-6 text-white">
-              Learn more <span aria-hidden="true">→</span>
-            </a>
+              <span className="sr-only">Open main menu</span>
+            </button>
+          </div>
+          <div className="hidden lg:flex lg:gap-x-12  ">
+            {navigation.map((item) => (
+              <a
+                key={item.name}
+                href={item.href}
+                className="text-sm font-semibold leading-6 text-gray-400"
+              >
+                {item.name}
+              </a>
+            ))}
+          </div>
+          <div className="hidden lg:flex lg:flex-1 lg:justify-end"></div>
+        </nav>
+        <Dialog
+          as="div"
+          className="lg:hidden"
+          open={mobileMenuOpen}
+          onClose={setMobileMenuOpen}
+        >
+          <div className="fixed inset-0 z-50" />
+          <Dialog.Panel className="fixed inset-y-0 right-0 z-50 w-full overflow-y-auto bg-white px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
+            <div className="flex items-center justify-between">
+              <a href="#" className="-m-1.5 p-1.5">
+                <img
+                  className="h-8 w-auto"
+                  src="https://tailwindui.com/img/logos/mark.svg?color=lime&shade=600"
+                  alt=""
+                />
+              </a>
+              <button
+                type="button"
+                className="-m-2.5 rounded-md p-2.5 text-gray-100"
+                onClick={() => setMobileMenuOpen(false)}
+              >
+                hello
+              </button>
+            </div>
+            <div className="mt-6 flow-root ">
+              <div className="-my-6 divide-y divide-gray-500/10">
+                <div className="space-y-2 py-6">
+                  {navigation.map((item) => (
+                    <a
+                      key={item.name}
+                      href={item.href}
+                      className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
+                    >
+                      {item.name}
+                    </a>
+                  ))}
+                </div>
+                <div className="py-6"></div>
+              </div>
+            </div>
+          </Dialog.Panel>
+        </Dialog>
+      </header>
+
+      <div className="relative isolate px-6  lg:px-8">
+        <div
+          className="absolute inset-x-0 -top-20 -z-10 transform-gpu overflow-hidden blur-3xl sm:-top-80"
+          aria-hidden="true"
+        >
+          <div
+            className="relative left-[calc(50%-11rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 rotate-[30deg] bg-gradient-to-tr from-[#c51db7] to-[#3b8efc] opacity-30 sm:left-[calc(50%-30rem)] sm:w-[72.1875rem]"
+            style={{
+              clipPath:
+                "polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)",
+            }}
+          />
+        </div>
+        <div className="mx-auto max-w-5xl py-32 sm:py-48 lg:py-56">
+          <div className="hidden sm:mb-8 sm:flex sm:justify-center">
+            <div className="relative rounded-full px-3 py-1 text-sm leading-6 text-gray-400 ring-1 ring-gray-500/10 hover:ring-gray-400/20 space-x-4 flex">
+              HI , MY NAME IS
+              <a href="#" className="font-semibold text-lime-600">
+                <span className="absolute inset-0" aria-hidden="true" />
+                <span aria-hidden="true">&rarr;</span>
+              </a>
+            </div>
+          </div>
+          <div className="text-center">
+            <h1 className=" text-3xl font-bold tracking-tight bg-gradient-to-r from-teal-500 via-lime-600 to-sky-600 bg-clip-text text-transparent sm:text-5xl lg:h-[4rem] md:h-[5rem] sm:h-[7rem]">
+              Abhishek Singh Chauhan{" "}
+              <span className="text-green-400 animate-pulse"> .</span>
+            </h1>
+            <motion.h1
+              initial={{ opacity: 0, y: 50 }}
+              animate={{ opacity: 1, y: 0 }}
+              transition={{ duration: 2 }}
+              className="text-4xl font-bold tracking-tight bg-gradient-to-r from-teal-500 via-lime-600 to-sky-600 bg-clip-text text-transparent sm:text-6xl "
+            >
+              I am a Frontend Developer
+            </motion.h1>
+            <p className="mt-6 text-lg leading-8 text-gray-300 ">
+              I Like to build amazing UI/UX experience for humans 🚀. Currently,
+              I'm working as a React Developer at OyeLabs.
+            </p>
+            <div className="mt-10 flex items-center justify-center gap-x-6">
+              <button className="lg:w-1/2 md:w-full sm:w-full bg-gradient-to-r from-teal-600 via-teal-800 to-gray-900 text-white font-semibold rounded-full p-3 hover:bg-teal-800 transition-transform duration-300 ease-in-out hover:scale-95">
+                Do You Want To Work With Me ?
+              </button>
+            </div>
           </div>
         </div>
-        <div className="mx-auto mt-16 flex max-w-2xl sm:mt-24 lg:ml-10 lg:mr-0 lg:mt-0 lg:max-w-none lg:flex-none xl:ml-32">
-          <div className="max-w-3xl flex-none sm:max-w-5xl lg:max-w-none">
-            <img
-              src="https://tailwindui.com/img/component-images/dark-project-app-screenshot.png"
-              alt="App screenshot"
-              width={2432}
-              height={1442}
-              className="w-[76rem] rounded-md bg-white/5 shadow-2xl ring-1 ring-white/10"
-            />
+
+        <div className="mx-auto max-w-7xl flex flex-col lg:flex-row justify-center text-gray-800 text-3xl lg:text-4xl lg:space-x-32 md:space-x-10 -top-7">
+          <div className="flex flex-col lg:flex-row  items-center mb-4 lg:mb-0">
+            <FaReact className="text-5xl lg:text-6xl text-gray-800 mb-2 mr-3" />
+            React
+          </div>
+          <div className="flex flex-col lg:flex-row  items-center mb-4 lg:mb-0">
+            <SiJavascript className="text-5xl lg:text-6xl text-gray-800 mr-3 mb-2" />
+            Javascript
+          </div>
+          <div className="flex flex-col lg:flex-row  items-center mb-4 lg:mb-0">
+            <SiTailwindcss className="text-5xl lg:text-6xl text-gray-800 mr-3 mb-2" />
+            Tailwind CSS
+          </div>
+          <div className="flex flex-col lg:flex-row  items-center mb-4 lg:mb-0">
+            <SiRedux className="text-5xl lg:text-6xl text-gray-800 mb-2 mr-3" />
+            Redux
           </div>
         </div>
       </div>
     </div>
   );
 };
-
 export default Hero;
--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -1,10 +1,9 @@
 import React from "react";
-import Navbar from "./Navbar";
+
 import Hero from "./Hero";
 const HomePage = () => {
   return (
     <div>
-      <Navbar />
       <Hero />
     </div>
   );
