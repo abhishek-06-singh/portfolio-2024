ecbe69b8dfb8a7e822a162e36e0aeb266a60d386
Abhishek singh chauhan <avi7000201383@gmail.com>
2024-05-21 13:57:36 +0530

    added annimation

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -96,7 +96,6 @@ const Hero = () => {
           aria-hidden="true"
         >
           <div
-            id="vanta"
             className="relative left-[calc(50%-11rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 rotate-[30deg] bg-gradient-to-tr from-[#c51db7] to-[#3b8efc] opacity-30 sm:left-[calc(50%-30rem)] sm:w-[72.1875rem]"
             style={{
               clipPath:
